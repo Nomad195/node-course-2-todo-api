8e2797efeac13d70d2fac64a541c3c4bec50ec02
Nomad195 <nomadpermanent@gmail.com>
2018-11-12 14:34:44 +0100

    'Add_GET_/todos/:id'

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+const {Todo} = require('./../server/models/todo');
+const {user} = require('./../server/models/user');
+const {ObjectID} = require('mongodb');
 
 
 var app = express();
@@ -30,6 +31,27 @@ Todo.find().then((todos) => {
 })
 });
 
+app.get('/todos/:id', (req, res) => {
+var id = req.params.id;
+
+if (!ObjectID.isValid(id)){
+    console.log('404 ID not valid');
+   return res.status(404).send();
+}
+
+user.findById(id).then((user) => {
+    if (!user){
+        res.status(404).send('id not found');
+        return console.log('Id not found');
+    }else{
+        res.send({user});
+        console.log('User By Id', user);
+    }
+    
+}, (e)  => {
+    res.status(400).send('id is incorrect');
+});
+});
 
 app.listen(3000, () => { 
 console.log('Server Started on port 3000');
